9b87bae67788e2c156f8abded24e9ff0e45316cd
ardazh <127732352+ardazh@users.noreply.github.com>
2023-03-30 17:36:12 +0700

    menambahkan data terakhir & finishing

--- table.js
+++ table.js
@@ -6,29 +6,47 @@ async function getData(){
 
     const row = function (el){
         return `
-        <tr class="border border-black">
-            <td class="border border-black">${el.id}</td>
-            <td class="border border-black">${el.name}</td>
-            <td class="border border-black">${el.username}</td>
-            <td class="border border-black">${el.email}</td>
-            <td class="border border-black">
-                    <div>
-                            <ul>
-                                <li>Street: ${el.address.street}</li>
-                                <li>Suite: ${el.address.suite}</li>
-                                <li>City: ${el.address.city}</li>
-                                <li>Zipcode: ${el.address.zipcode}</li>
-                                <li>geo: </li>
-                                <div class="ml-3">
-                                    <li>
-                                        <ul>
-                                            <li>lat: ${el.address.geo.lat}</li>
-                                            <li>lng: ${el.address.geo.lng}</li>
-                                        </ul>
-                                    </li>
-                                </div>
-                            </ul>
-                    </div>
+        <tr class="border border-black text-sm">
+            <td class="border border-black p-2 w-10 text-center">${el.id}</td>
+            <td class="border border-black p-2">${el.name}</td>
+            <td class="border border-black p-2">${el.username}</td>
+            <td class="border border-black p-2">${el.email}</td>
+            <td class="border border-black p-2">
+                <div>
+                    <ul>
+                        <li class="p-2">
+                            <span class="font-bold">Street </span> 
+                            ${el.address.street}
+                        </li>
+                        <li class="p-2">
+                            <span class="font-bold">Suite </span> 
+                            ${el.address.suite}
+                        </li>
+                        <li class="p-2">
+                            <span class="font-bold">City </span> 
+                            ${el.address.city}
+                        </li>
+                        <li class="p-2">
+                            <span class="font-bold">Zip Code </span> 
+                            ${el.address.zipcode}
+                        </li>
+                        <li class="p-2">
+                            <span class="font-bold">Geo </span> 
+                            ${el.address.geo.lat} ${el.address.geo.lng}
+                        </li>
+                    </ul>
+                </div>
+            </td>
+            <td class="border border-black w-48 p-2">${el.phone}</td>
+            <td class="border border-black p-2">${el.website}</td>
+            <td class="border border-black p-2">
+                <div>
+                    <ul class="gap-3">
+                        <li class="font-bold">${el.company.name}</li>
+                        <li> ${el.company.catchPhrase}</li>
+                        <li> ${el.company.bs}</li>
+                    </ul>
+                </div>    
             </td>
         </tr>
         `
